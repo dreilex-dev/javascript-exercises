8e1128965709e22b2e7102d94d8c1c797f92789c
dreilex <dreilex@outlook.com>
2024-07-08 23:53:51 +0200

    fibonacci program deployed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+function fibonacci(n) {
+    // Convertim n la număr întreg dacă este șir de caractere
+    n = parseInt(n, 10);
 
-};
+    // Verificăm dacă n este un număr întreg pozitiv sau zero
+    if (isNaN(n) || n < 0) {
+        return "OOPS";
+    }
 
-// Do not edit below this line
+    // Inițializăm primele două numere din secvența Fibonacci
+    let a = 0, b = 1;
+
+    // Generăm secvența Fibonacci până la al n-lea număr
+    for (let i = 1; i < n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    // Returnăm al n-lea număr Fibonacci
+    return n === 0 ? 0 : b;
+}
+
 module.exports = fibonacci;
