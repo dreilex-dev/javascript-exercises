6fa9229df20a59fba87bcb850623f719754d4461
dreilex <dreilex@outlook.com>
2024-07-08 19:09:56 +0200

    fix: exercises solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  const array = Array.from({ length: num }, (v, k) => k + 1);
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
 // Do not edit below this line
