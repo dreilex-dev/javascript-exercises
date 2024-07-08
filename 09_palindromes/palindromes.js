8654a4ede9b2a4083f5bb30521d1c36cc516cc43
dreilex <dreilex@outlook.com>
2024-07-08 21:48:59 +0200

    palindrome program deployed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+function palindromes(text) {
+    // Normalizează textul: elimină toate caracterele non-alfanumerice și convertește la litere mici
+    const normalizedText = text.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+    // Verifică dacă textul este un palindrom
+    const length = normalizedText.length;
+    for (let i = 0; i < length / 2; i++) {
+      if (normalizedText[i] !== normalizedText[length - 1 - i]) {
+        return false; // Dacă găsești o neconcordanță, nu este palindrom
+      }
+    }
+    return true; // Dacă toate caracterele se potrivesc, este palindrom
+  }
+  
+  module.exports = palindromes;
+  
